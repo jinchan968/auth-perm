6bf9a4c95adf8045cd52c6d7a98e5a3ffdf0a30d
looper <looper@MacBookAir.local>
2026-02-18 18:31:40 +0800

    add tenant code-gen infra dependencies

--- internal/domain/tenant/module.go
+++ internal/domain/tenant/module.go
@@ -11,8 +11,6 @@ import (
 	"auth-perm/internal/domain/tenant/service"
 )
 
-// dependencies.go 定义了租户领域的依赖注册函数
-
 // RegisterTenantDomain 注册租户领域的所有依赖
 func RegisterTenantDomain(container *dig.Container) error {
 	// -----------------------------------------------------
